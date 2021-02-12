e13a08f0a5f8de5e0d60297849a1c5e4e60683ff
pndnism <p.dai.man8@gmail.com>
2021-02-12 10:05:46 +0900

    update sql_for_preparation_features

--- query/prepare_features/prepare_features.sql
+++ query/prepare_features/prepare_features.sql
@@ -3,7 +3,7 @@ with
     race_info as (
         SELECT
             race_id,
-            horse_ids as horse_id,
+            cast(horse_ids as string) as horse_id,
             arrival_orders,
             box_numbers,
             horse_numbers,
@@ -48,16 +48,54 @@ with
             father_of_mother
         FROM horse_race_prediction.horse_stackings
     ),
-
+    -- レコードを中央競馬に絞る
     center_race_info as (
         select 
             * 
         from race_info
         where held_place in ("札幌","函館","福島","新潟","中山","東京","中京","京都","阪神","小倉")
-        )
+        ),
+    base_categorical_features as (
+        select 
+            race_id,
+            horse_id,
+            box_numbers,
+            horse_numbers,
+            horse_sexes,
+            jockey_names,
+            trainer_names,
+            horse_owners,
+            race_number,
+            course_type,
+            course_direction,
+            weather,
+            course_condition,
+            race_start_time,
+            REGEXP_EXTRACT(held_date, r'\d+月') as held_month,
+            EXTRACT(DAYOFWEEK from DATE(cast(REGEXP_EXTRACT(held_date, r'(\d+)年') as int64), cast(REGEXP_EXTRACT(held_date, r'(\d+)月') as int64),cast(REGEXP_EXTRACT(held_date, r'(\d+)日') as int64))) as held_day_of_week,
+            held_place,
+            case 
+              when course_length < 1400 then '短距離'
+              when course_length < 1800 then 'マイル'
+              when course_length < 2200 then '中距離'
+              when course_length < 2800 then '中長距離'
+              else '長距離'
+            end course_length_category,
+            producer,
+            origin_place,
+            mother,
+            father,
+            mother_of_mother,
+            father_of_mother,
+            mother_of_father,
+            father_of_father
+            from center_race_info
+            left outer join horse_info
+            using (horse_id)
+    )
 
-select 
-    race_id,
-    horse_id,
-    cast(SPLIT(goal_times, ":")[OFFSET(0)] as float64) * 60 + cast(SPLIT(goal_times, ":")[OFFSET(1)] as float64) as goal_time
-from center_race_info
+select * from base_categorical_features LIMIT 1000
+
+# select course_length, count(*) from center_race_info
+# group by course_length
+# order by course_length
